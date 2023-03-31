465620c5918c9c435db36877ea57bfc9625f16ee
booky10 <boooky10@gmail.com>
2023-04-01 01:19:11 +0200

    Make launchplates a separate plugin

--- src/main/java/dev/booky/cloudcore/CloudCoreMain.java
+++ src/main/java/dev/booky/cloudcore/CloudCoreMain.java
@@ -1,19 +1,10 @@
 package dev.booky.cloudcore;
 // Created by booky10 in CloudCore (10:35 14.03.23)
 
-import dev.booky.cloudcore.commands.CloudCoreCommand;
-import dev.booky.cloudcore.commands.LaunchPlateCommand;
-import dev.booky.cloudcore.listener.LaunchPlateListener;
-import dev.booky.cloudcore.util.TranslationLoader;
 import org.bstats.bukkit.Metrics;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CloudCoreMain extends JavaPlugin {
-
-    private CloudManager manager;
-    private TranslationLoader i18n;
 
     public CloudCoreMain() {
         try {
@@ -26,28 +17,5 @@ public class CloudCoreMain extends JavaPlugin {
     @Override
     public void onLoad() {
         new Metrics(this, 17949);
-        this.manager = new CloudManager(this);
-
-        this.i18n = new TranslationLoader(this);
-        this.i18n.load();
-    }
-
-    @Override
-    public void onEnable() {
-        this.manager.reloadConfig();
-
-        Bukkit.getPluginManager().registerEvents(new LaunchPlateListener(this.manager), this);
-
-        LaunchPlateCommand.register(this.manager);
-        CloudCoreCommand.register(this.manager);
-
-        Bukkit.getServicesManager().register(CloudManager.class, this.manager, this, ServicePriority.Normal);
-    }
-
-    @Override
-    public void onDisable() {
-        if (this.i18n != null) {
-            this.i18n.unload();
-        }
     }
 }
